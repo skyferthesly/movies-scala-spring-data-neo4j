42c40af32a3fae334709e35febde0f199edb1f4a
Moore-Firkins <skyler.moore-firkins@certilytics.com>
2018-04-04 11:24:24 -0400

    fixed main constructor

--- src/main/scala/movies/MoviesApplication.scala
+++ src/main/scala/movies/MoviesApplication.scala
@@ -8,16 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 
 @SpringBootApplication
-class MoviesApplication {
-    @Autowired
-    var repo: MovieRepository = _
-    var configuration: RepositoryRestConfiguration = _
-
-    def this(config: RepositoryRestConfiguration) = {
-        this()
-        this.configuration = config
-    }
-
+class MoviesApplication(var configuration: RepositoryRestConfiguration) {
     @PostConstruct
     def config: Unit = this.configuration.exposeIdsFor(classOf[Movie])
 }
@@ -25,10 +16,4 @@ class MoviesApplication {
 //@EntityScan(Array("movies"))
 object MoviesApplication {
    def main(args: Array[String]): Unit = SpringApplication.run(classOf[MoviesApplication])
-    
-   
-
-//     //@PostConstruct
-//     //def thing: Unit = repo.save(new Movie("movie1", 1991, "lame af"))
-    
  }
--- src/main/scala/movies/configuration/ExposeIdsConfiguration.scala
+++ src/main/scala/movies/configuration/ExposeIdsConfiguration.scala
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 
-@Component
+//@Component
 class ExposeIdsConfiguration extends RepositoryRestConfigurerAdapter {
   override def configureRepositoryRestConfiguration(config: RepositoryRestConfiguration): Unit = config.exposeIdsFor(classOf[Movie])
   
